580956851ee3e6d71bb89a3097285592975d4893
Seva <15mymoney15@gmail.com>
2021-10-21 15:31:26 +0300

    Основа ajax 1 + команды гит

---
+++ git commands.php
@@ -0,0 +1,7 @@
+echo "# ajax" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SevaPHP/ajax.git
+git push -u origin main
